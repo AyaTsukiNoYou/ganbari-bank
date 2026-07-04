d5ff216e38b5b4253579dcf2103153ea0d72fbd0
AyaTsukiNoYou <sunny0sun30@gmail.com>
2026-07-04 19:55:38 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+/* オフラインでも動くようにするサービスワーカー */
+const CACHE = 'ganbari-pub-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  // アプリ本体はキャッシュ優先、それ以外(YouTube等)はそのまま
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
